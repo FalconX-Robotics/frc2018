96868a1a5db10a3c73ed7b28470eb5e92c12de7e
GabriellaYang <34780010+GabriellaYang@users.noreply.github.com>
2018-01-31 16:42:53 -0800

    Created new Command
    Created new command to drive an inputed distance to be used in autonomous

---
+++ src/org/usfirst/frc/team6662/robot/commands/DriveSetDistance.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team6662.robot.commands;
+
+import org.usfirst.frc.team6662.robot.Robot;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriveSetDistance extends Command {
+	public static final double SPEED = 1;
+
+	private Encoder leftEncoder = Robot.drivetrain.getLeftEncoder();
+	private Encoder rightEncoder = Robot.drivetrain.getRightEncoder();
+	private int distance = 0;
+	
+    public DriveSetDistance(int distance) {
+       requires(Robot.drivetrain);
+       this.distance = distance;
+    }
+
+    protected void execute() {
+    	Robot.drivetrain.tankDrive(SPEED,SPEED);
+    }
+
+    protected boolean isFinished() {
+        return leftEncoder.getDistance() <= distance && rightEncoder.getDistance() <= distance;
+    }
+}
