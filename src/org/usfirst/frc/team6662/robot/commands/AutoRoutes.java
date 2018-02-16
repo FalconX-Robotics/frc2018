25269f56525fcce9245fe6c5297b5a8ed7163f72
myahn725 <34779979+myahn725@users.noreply.github.com>
2018-02-15 18:03:03 -0800

    Determined playing field values
    also switched out DriveSetDistance for DriveForward
    hasn't been debugged yet
    most importantly, we have major issues concerning our current autonomous plan, please check up on issues

--- src/org/usfirst/frc/team6662/robot/commands/AutoRoutes.java
+++ src/org/usfirst/frc/team6662/robot/commands/AutoRoutes.java
@@ -1,66 +1,61 @@
-package org.usfirst.frc.team6662.robot.commands;
+package org.usfirst.frc.team6662.robot.commands; //LET'S FIND THE ADJACENT DISTANCES! (AutoRoutes.getAdjacent(char position))
 
-import edu.wpi.first.wpilibj.command.CommandGroup;//only works for right side so far...
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class AutoRoutes extends CommandGroup {
 	
-	private static final int wallToSwitch = 101;//actual distance is 101.25 inches
-	private static final int wallToScale = 1; //these distances are undetermined
-	private static final int switchToSide = 1;//wall means the alliance wall and side is the right side of the playing field
-	private static final int scaleToSide = 1;
-	private static final int initialDriveForward = 30;
+	private static final double wallToSwitch = 168;//actual distance is 101.25 inches
+	private static final double wallToScale = 324; //these distances are undetermined
+	private static final double switchToSide = 90;//wall means the alliance wall and side is the right side of the playing field
+	private static final double scaleToSide = 72;
+	private static final double initialDriveForward = 30;
 	
-	private static int distance;//I made this variable because I couldn't get AutoRoutes.getAdjacent to work for some reason
+	private static double distance;//I made this variable because I couldn't get AutoRoutes.getAdjacent to work for some reason
 
     public AutoRoutes(char position, boolean Scale, char ScaleSide, char SwitchSide) {
     	
-    	int adjacent = AutoRoutes.getAdjacent(position);// thinking of adjacent as in opposite, adjacent, hypotenuse
+    	double adjacent = AutoRoutes.getAdjacent(position);// thinking of adjacent as in opposite, adjacent, hypotenuse
     	double firstAngle = AutoRoutes.getFirstAngle(adjacent);//diagonal distance is hypotenuse, wallToSwitch/Scale is opposite
     	
-    	addSequential(new DriveSetDistance(initialDriveForward));//drive forward so we can turn w/o bumping into the wall
+    	addSequential(new DriveForward(initialDriveForward, 0.7));//drive forward so we can turn w/o bumping into the wall
     	addSequential(new TurnAngle(firstAngle, 0.7));
-    	addSequential(new DriveSetDistance(AutoRoutes.getFirstDistance(adjacent)));//this takes us to wall next to the switch
+    	addSequential(new DriveForward(AutoRoutes.getFirstDistance(adjacent), 0.7));//this takes us to wall next to the switch
     	
     	if (Scale == true){
-	
 		if (ScaleSide == 'R'){
     			addSequential(new TurnAngle(AutoRoutes.getSecondAngleForScale(firstAngle), 0.7));//make parallel to wall
-    			addSequential(new DriveSetDistance(AutoRoutes.getSecondDistanceForScale()));//drive along wall until scale
+    			addSequential(new DriveForward(AutoRoutes.getSecondDistanceForScale(), 0.7));//drive along wall until scale
     			addSequential(new TurnAngle(AutoRoutes.getThirdAngle(), 0.7));//make aligned with scale
-    			addSequential(new DriveSetDistance(scaleToSide));//robot approaches from null territory
+    			addSequential(new DriveForward(scaleToSide), 0.7);//robot approaches from null territory
 		}
 		else {
 			addSequential(new TurnAngle(AutoRoutes.getSecondAngleForScale(-1*firstAngle), 0.7));//make parallel to wall
-    			addSequential(new DriveSetDistance(AutoRoutes.getSecondDistanceForScale()));//drive along wall until scale
+    			addSequential(new DriveForward(AutoRoutes.getSecondDistanceForScale(), 0.7));//drive along wall until scale
     			addSequential(new TurnAngle(-1*AutoRoutes.getThirdAngle(), 0.7));//make aligned with scale
-    			addSequential(new DriveSetDistance(scaleToSide));//robot approaches from null territory
+    			addSequential(new DriveForward(scaleToSide), 0.7);//robot approaches from null territory
 		}
-		
     	}
-    	
     	else {
-	
 		if (SwitchSide == 'R'){
     			addSequential(new TurnAngle(AutoRoutes.getSecondAngleForSwitch(firstAngle), 0.7));//align with switch
-    			addSequential(new DriveSetDistance(switchToSide));approach switch from the right
+    			addSequential(new DriveForward(switchToSide), 0.7);approach switch from the right
 		}
 		else {
 			addSequential(new TurnAngle(-1*Routes.getSecondAngleForSwitch(firstAngle), 0.7));//align with switch
-    			addSequential(new DriveSetDistance(switchToSide));approach switch from the right
+    			addSequential(new DriveForward(switchToSide), 0.7);approach switch from the right
     		}
     }
     
-    public static int getFirstDistance(int angle){
-    	return (int) Math.sqrt(Math.pow(angle, 2) + Math.pow(wallToSwitch - initialDriveForward, 2));
+    public static double getFirstDistance(double angle){
+    	return Math.sqrt(Math.pow(angle, 2) + Math.pow(wallToSwitch - initialDriveForward, 2));
 	//using pythagorean theorem to find the distance needed to travel^
     }
     
-    public static int getSecondDistanceForScale(){
+    public static double getSecondDistanceForScale(){
     	return wallToScale - wallToSwitch - initialDriveForward;//distance that we have to drive from switch to scale
     }
     
-    public static int getAdjacent(char position){//getting the distance from robot to right wall
-    	
+    public static double getAdjacent(char position){//getting the distance from robot to right wall	
     	if(position == 'a'){
     		distance = 1;}//distances are undetermined
     	else if(position == 'b'){
@@ -68,10 +63,9 @@ public class AutoRoutes extends CommandGroup {
     	else if(position == 'c'){
     		distance = 1;}
     	return distance;
-    	
-    	}
+    }
     
-    public static double getFirstAngle(int adjacent){
+    public static double getFirstAngle(double adjacent){
     	return Math.atan((wallToSwitch-initialDriveForward)/adjacent);//basically, tan^-1(opposite/adjacent) = angle
     }
     
@@ -83,7 +77,7 @@ public class AutoRoutes extends CommandGroup {
     	return -1*(90 + angle);
     }
     
-    public static int getThirdAngle(){
+    public static double getThirdAngle(){
     	return -90;
     }
 }
