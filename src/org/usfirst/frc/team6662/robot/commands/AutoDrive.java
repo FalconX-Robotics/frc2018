8e9b193149c91529b3089983cb2cac069f9743ef
bkong <kongbrian02@gmail.com>
2018-02-17 17:21:35 -0800

    Add switch statements for flexiblity: Can go to either switch or scale, but only one.

--- src/org/usfirst/frc/team6662/robot/commands/AutoDrive.java
+++ src/org/usfirst/frc/team6662/robot/commands/AutoDrive.java
@@ -8,11 +8,12 @@ public class AutoDrive extends CommandGroup {
 	private double initDistance = 2.5;
 	private double distanceToInitElementPosition = 0.0; //calculated
 	private double distanceToElement = 0.0;//perpendicular distance from alliance wall edge to switch
-	private double WallAndSwitchDist = 8.4375;//distance to drive to switch
+	private double distanceFromOppToElement = 0.0;//distance to drive from driving the wall to the game element
 	private double turnA = 0.0;//turn on point A (start of distanceA)
 	private double turnC = 90.0;//turn to point C(end of distanceB)
-	double adj = 54;
-	
+	double position = 0.0;
+	double distanceToElements[] = {13.6, 17};
+	double distanceFromOppositeToElements[] = {7, 5.4375};
 	public double modTurns (double angle, char side) {
 		if(side == 'L') {
 			return angle *-1;
@@ -22,26 +23,42 @@ public class AutoDrive extends CommandGroup {
 		}
 	}
 	
-	public AutoDrive(double opp, double adj, double targetDistance, char side) { //use RobotMap for the side
+	public AutoDrive(double opp, double position, String element, char side) { //use RobotMap for the side
 		super("AutoDrive");
 		//opp is the point A's height difference from the start (after startDistance)
 		//adj is the pure horizontal distance from wall
+		this.position = position;
 		
 		if (side == 'L') {
-			adj = 27 - adj;
+			this.position = 27 - position;
+		}
+
+		this.turnA = 90-Math.atan2(opp, position);
+		this.distanceToInitElementPosition = Math.hypot(opp, position);
+		
+		switch (element) {
+		case "switch":
+			this.distanceToElement = distanceToElements[0];
+			this.distanceFromOppToElement = distanceFromOppositeToElements[0];
+			break;
+		case "scale":
+			this.distanceToElement = distanceToElements[1];
+			this.distanceFromOppToElement = distanceFromOppositeToElements[1];
+			break;
 		}
 		
-		this.adj = adj;
-		this.turnA = 90-Math.atan2(opp, adj);
-		this.distanceToInitElementPosition = Math.sqrt(Math.pow(opp, 2) + Math.pow(adj, 2.0));
-		this.distanceToElement = targetDistance - 3.0;//adjust distanceB to whether it's for the switch or scale
-    		
+		this.distanceToElement = distanceToElements[1];
+		if (element == "switch") {
+			this.distanceToElement = distanceToElements[0];
+		}
+		this.distanceToElement -=initDistance;
+		
 		addSequential(new DriveForward(initDistance, 1));//
 		addSequential(new TurnAngle(modTurns(turnA, side), 0.5));//
 		addSequential(new DriveForward(distanceToInitElementPosition, 1));//
 		addSequential(new TurnAngle(0.0, 0.5));//
 		addSequential(new DriveForward(distanceToElement, 1));
 		addSequential(new TurnAngle(modTurns(turnC, side), 0.5));
-		addSequential(new DriveForward(WallAndSwitchDist, 1));
+		addSequential(new DriveForward(distanceFromOppToElement, 1));
 	}
 }
