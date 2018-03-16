745860ed9d93e35637befe7fc669812681c84ce7
bkong <kongbrian02@gmail.com>
2018-03-15 22:01:04 -0700

    Alter DriveDistance code

--- src/org/usfirst/frc/team6662/robot/commands/DriveDistance.java
+++ src/org/usfirst/frc/team6662/robot/commands/DriveDistance.java
@@ -8,39 +8,42 @@ import edu.wpi.first.wpilibj.command.Command;
 public class DriveDistance extends Command {
 	private double targetDistance = 0;
 	private double targetPosition = 0;
+	protected boolean finished = false;
 	
 	public DriveDistance(double targetDistance) {
 		super("Drive " + targetDistance + 
-				(targetDistance == 1 ? " inch " : " inches ") + 
+				(Math.abs(targetDistance) == 1 ? " inch " : " inches ") + 
 				(targetDistance > 0 ? "forward" : "backward"));
 		requires(Robot.drivetrain);
 		
-		double targetRotations = targetDistance / Drivetrain.WHEEL_CIRCUMFERENCE;
 		this.targetDistance = targetDistance;
+		double targetRotations = targetDistance / Drivetrain.WHEEL_CIRCUMFERENCE;
 		this.targetPosition = targetRotations * 
 				Drivetrain.ENCODER_UNITS_PER_ROTATION / Drivetrain.SHAFT_RATIO;
 	}
 	
 	@Override
 	protected void initialize() {
+		finished = false;
 		Robot.drivetrain.zeroPosition();
 	}
 	
 	@Override
 	protected void execute() {
-		Robot.drivetrain.driveDistance(targetDistance);
-		
+		Robot.drivetrain.tankDrive(0.8 , 0.8);
 		//System.out.println("Current: " + Robot.drivetrain.getCurrentPosition());
 		//System.out.println("Target: " + targetPosition);
+		finished = Math.abs(Robot.drivetrain.getCurrentPosition()) >= Math.abs(targetPosition);
+	}
+	
+	@Override
+	protected boolean isFinished() {
+		return finished;
 	}
 	
 	@Override
 	protected void end() {
 		Robot.drivetrain.zeroPosition();
-	}
-	
-	@Override
-	protected boolean isFinished() {
-		return Math.abs(Robot.drivetrain.getCurrentPosition()) >= Math.abs(targetPosition);
+		Robot.drivetrain.tankDrive(0.0, 0.0);
 	}
 }
